2eb11a7562bc925d06db8b44fc9eef26a6f41a37
ramezani13 <147407440+ramezani13@users.noreply.github.com>
2025-11-27 11:52:50 +0100

    mask_and_classify_scores.py

--- is_even.py
+++
@@ -1,7 +0,0 @@
-def is_even(k):
-    """
-    Return True if k is even, otherwise return False.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
